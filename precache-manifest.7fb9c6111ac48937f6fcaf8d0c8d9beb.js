ca399d34d9ccb42f6da16ad43775988748167b66
Swapnil Rajawat <swapnilrajawat1512@gmail.com>
2020-06-20 22:30:31 +0530

    Updates

---
+++ precache-manifest.7fb9c6111ac48937f6fcaf8d0c8d9beb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7e1b8385fbf238e6c06f44b1a13fd1",
+    "url": "/React-Based-Timeline/index.html"
+  },
+  {
+    "revision": "1717dc4896973d412178",
+    "url": "/React-Based-Timeline/static/css/main.98cffefd.chunk.css"
+  },
+  {
+    "revision": "099640b21f5887d90f1f",
+    "url": "/React-Based-Timeline/static/js/2.0443239c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Based-Timeline/static/js/2.0443239c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1717dc4896973d412178",
+    "url": "/React-Based-Timeline/static/js/main.19691581.chunk.js"
+  },
+  {
+    "revision": "a06bec9d2c1420f8b97a",
+    "url": "/React-Based-Timeline/static/js/runtime-main.cb60d4dc.js"
+  },
+  {
+    "revision": "314c4659ba8452cc97190c72676828e2",
+    "url": "/React-Based-Timeline/static/media/minimal2.314c4659.jpg"
+  },
+  {
+    "revision": "a8dbdd1d6f4b21320e9655e9402e2812",
+    "url": "/React-Based-Timeline/static/media/minimal5.a8dbdd1d.jpg"
+  },
+  {
+    "revision": "1b419d03f35318e88ddde6e1235b8e5d",
+    "url": "/React-Based-Timeline/static/media/timeline.1b419d03.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40d4fce2591a94c2d0e2592829c93b50.js"
+  "/React-Based-Timeline/precache-manifest.7fb9c6111ac48937f6fcaf8d0c8d9beb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Based-Timeline/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
